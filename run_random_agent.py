8e54cc03df9f772d7d2b7091614d5e247aa0395e
iandreariley <iandreariley@gmail.com>
2017-05-04 16:43:17 -0700

    Fix print statement.

--- run_random_agent.py
+++ run_random_agent.py
@@ -8,9 +8,9 @@ COMMANDS = "minimal"
 AUTO_RESTART = True
 
 class RandoCommando(object):
-"""
-If he gets the job done, it's totally by accident.
-"""
+    """
+    If he gets the job done, it's totally by accident.
+    """
     
     def __init__(self):
         super()
@@ -26,17 +26,22 @@ if __name__ == '__main__':
     env = gym.make(args.env)
     player = RandoCommando()
     actions = env.action_space
+    total_reward = 0
 
-    while True:
+    for _ in range(100):
         state = env.reset()
         terminal = False
-        total_reward = 0
+        episode_reward = 0
         steps = 0
         while not terminal:
             env.render()
             action = player.act(state, actions)
             state, reward, terminal, info = env.step(action)
             steps += 1
-            total_reward += 1
+            episode_reward += reward
             if terminal:
-                print("episode terminated after %d steps with a total reward of %g" % (steps, total_reward))
+                print("episode terminated after %d steps with a total reward of %g" % (steps, episode_reward))
+                total_reward += episode_reward
+
+    print("Avg reward: %g" % float(total_reward) / 100.0)
+    
