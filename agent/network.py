a20b22fc2c6523937f332044421eaa484833c820
iandreariley <iandreariley@gmail.com>
2017-05-18 12:53:58 -0700

    Add the rest of the dense network for cartpole.

--- agent/network.py
+++ agent/network.py
@@ -120,3 +120,29 @@ class Dense_AC_Network():
             weights_initializer=normalized_columns_initializer(1.0),
             biases_initializer=None)
 
+        
+        #Only the worker network need ops for loss functions and gradient updating.
+        if scope != 'global':
+            self.actions = tf.placeholder(shape=[None], dtype=tf.int32)
+            self.actions_onehot = tf.one_hot(self.actions, a_size, dtype=tf.float32)
+            self.target_v = tf.placeholder(shape=[None], dtype=tf.float32)
+            self.advantages = tf.placeholder(shape=[None], dtype=tf.float32)
+
+            self.responsible_outputs = tf.reduce_sum(self.policy * self.actions_onehot, [1])
+
+            #Loss functions
+            self.value_loss = 0.5 * tf.reduce_sum(tf.square(self.target_v - tf.reshape(self.value, [-1])))
+            self.entropy = -tf.reduce_sum(self.policy * tf.log(self.policy))
+            self.policy_loss = -tf.reduce_sum(tf.log(self.responsible_outputs) * self.advantages)
+            self.loss = 0.5 * self.value_loss + self.policy_loss - self.entropy * 0.01
+
+            #Get gradients from local network using local losses
+            local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
+            self.gradients = tf.gradients(self.loss, local_vars)
+            self.var_norms = tf.global_norm(local_vars)
+            grads, self.grad_norms = tf.clip_by_global_norm(self.gradients, 40.0)
+
+            #Apply local gradients to global network
+            global_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'global')
+            self.apply_grads = trainer.apply_gradients(zip(grads, global_vars))
+
